b56c3e1fc77986737ef39c58fd47eca4d07cc006
Moath <moath.dlaii@gmail.com>
2021-03-23 00:33:06 -0500

    refactored square component, destructuring props

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,13 @@
-export const Square = (props) => {
+export const Square = ({ value, onClick }) => {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button className="square" onClick={onClick}>
+      {value}
     </button>
   );
 };
+
+// Destructuring our props instead of passing props then do props.onClick, props.value...like below
+
+/* <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button> */
